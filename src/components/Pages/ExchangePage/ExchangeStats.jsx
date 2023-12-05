c3c231bdf55c37d42c842d8c9b5dfb0cb68dee81
medinalicina <medinalicina94@gmail.com>
2023-12-05 20:48:27 +0100

    za amina

--- src/components/Pages/ExchangePage/ExchangeStats.jsx
+++ src/components/Pages/ExchangePage/ExchangeStats.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import './ExchangeStats.css'
-function ExchangeStats({exchanges}) {
+import React from 'react';
+import './ExchangeStats.css';
+
+function ExchangeStats({ exchanges }) {
+  const price = parseFloat(exchanges.price);
 
   return (
-    <div className='stats-content'>
-        <p>{exchanges.rank}.</p>
-        <img src={exchanges.iconUrl}></img>
-        <p>{exchanges.name}</p>
-        <p>{exchanges.price}</p>
+    <div className='stats-content1'>
+      <p>{exchanges.rank}.</p>
+      <img src={exchanges.iconUrl} alt={exchanges.name}></img>
+      <p>{exchanges.name}</p>
+      <p id='price'>{price.toFixed(2)}</p>
     </div>
-  )
+  );
 }
 
-export default ExchangeStats
+export default ExchangeStats;
