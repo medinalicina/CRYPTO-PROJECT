d59c49a4f6ffbcaaee3d82e4fc1faaf9d0e4b2aa
medinalicina <medinalicina94@gmail.com>
2023-12-09 17:54:00 +0100

    hubby amin

--- src/components/Card/CryptoInfo.jsx
+++ src/components/Card/CryptoInfo.jsx
@@ -23,19 +23,9 @@ function CryptoInfo({ coin }) {
         Last 24h: <span style={{ color: textColor }}>{formattedChange}</span>
       </p>
       <p className="info-text">MarketCap: {coin.marketCap}</p>
-      <p className="info-text">Price: {numericPrice}</p>
+      <p className="info-text">Price : {numericPrice}</p>
     </div>
-    <div className="crypto-info">
-    <p className="info-text">
-      Name: {coin.name} ({coin.symbol})
-    </p>
-  
-    <p className="info-text">
-      Last 24h: <span style={{ color: textColor }}>{formattedChange}</span>
-    </p>
-    <p className="info-text">MarketCap: {coin.marketCap}</p>
-    <p className="info-text">Price: {numericPrice}</p>
-  </div>
+    
   );
 }
 
